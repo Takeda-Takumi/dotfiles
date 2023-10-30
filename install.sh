668fd09faaa0c26b4dd1849564c4ae8c12e8161f
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-10-30 13:12:58 +0900

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
-echo "install.sh"
+#!/bin/sh
+
+if test ! "$(which brew)"
+then
+    if test "$(uname)" = "Darwin"
+    then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    elif test "$(uname -s | cut -c 1-5 )" = "Linux"
+    then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    fi
+fi
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install stow
+
+stow -v nvim
