997325499b77529740438b1d953949044b68ddc7
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-15 16:49:55 +0900

    add options in lua

--- nvim/.config/nvim/lua/config/option.lua
+++ nvim/.config/nvim/lua/config/option.lua
@@ -1,3 +1,37 @@
+-- view
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.list = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrapscan = true
+
+-- completion
+vim.opt.completeopt = { 'menu' }
+vim.opt.infercase = true
+vim.opt.wildmenu = true
+
+--  text edit
+vim.opt.hidden = true
+vim.opt.textwidth = 0
+vim.opt.encoding = 'utf-8'
+vim.cmd.syntax('enable')
+
+-- log
+vim.opt.history = 500
+vim.opt.swapfile = false
+vim.opt.undofile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.viminfo = ''
+
+-- other
+vim.opt.helplang = { 'ja', 'en' }
+
+-- colorschme
 vim.cmd [[
 try
   colorscheme tokyonight
