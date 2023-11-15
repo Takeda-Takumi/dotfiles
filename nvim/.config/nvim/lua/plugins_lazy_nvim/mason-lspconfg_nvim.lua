434ca2d298ecd482c6a8091138686e1b57e2526b
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-15 23:06:50 +0900

    add nvim-cmp

--- nvim/.config/nvim/lua/plugins_lazy_nvim/mason-lspconfg_nvim.lua
+++ nvim/.config/nvim/lua/plugins_lazy_nvim/mason-lspconfg_nvim.lua
@@ -8,12 +8,15 @@ return {
   event = { 'VimEnter' },
 
   config = function()
+    local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
     require('mason')
     local mason_lspconfig = require('mason-lspconfig')
     mason_lspconfig.setup()
     mason_lspconfig.setup_handlers {
       function(server_name)
-        require('lspconfig')[server_name].setup {}
+        require('lspconfig')[server_name].setup {
+          capabilities = default_capabilities,
+        }
       end
     }
   end,
