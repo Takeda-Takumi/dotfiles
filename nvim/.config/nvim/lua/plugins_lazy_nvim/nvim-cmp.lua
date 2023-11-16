a59ad9610030cffbb6c11c052460fb15c56e9a2f
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-16 14:00:13 +0900

    use divicons in nvim-cmp

--- nvim/.config/nvim/lua/plugins_lazy_nvim/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins_lazy_nvim/nvim-cmp.lua
@@ -12,6 +12,7 @@ return {
     'onsails/lspkind.nvim',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lua',
+    'nvim-tree/nvim-web-devicons'
   },
 
   config = function()
@@ -79,17 +80,17 @@ return {
         { name = 'nvim_lua' },
       }),
       formatting = {
-        format = require('lspkind').cmp_format({
-          mode = 'symbol',       -- show only symbol annotations
-          maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-          -- The function below will be called before any actual modifications from lspkind
-          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-          before = function(entry, vim_item)
-            return vim_item
+        format = function(entry, vim_item)
+          if vim.tbl_contains({ 'path' }, entry.source.name) then
+            local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+            if icon then
+              vim_item.kind = icon
+              vim_item.kind_hl_group = hl_group
+              return vim_item
+            end
           end
-        })
+          return require('lspkind').cmp_format({ with_text = true, })(entry, vim_item)
+        end
       }
     })
 
