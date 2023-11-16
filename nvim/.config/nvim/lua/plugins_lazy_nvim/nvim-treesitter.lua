b4cae5c5ee2b2068b4bb7fe356b56e7dac79e4f5
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-16 10:07:18 +0900

    add nvim-treesitter

---
+++ nvim/.config/nvim/lua/plugins_lazy_nvim/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = 'BufEnter',
+
+  dependencies = {
+    'yioneko/nvim-yati',
+  },
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      sync_install = false,
+      highlight = { enable = false },
+      indent = { enable = false },
+    })
+  end
+}
