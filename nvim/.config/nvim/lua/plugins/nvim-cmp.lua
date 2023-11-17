d9fab2cc5fc0d1199a37378de0ef9162e6e0509d
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-17 23:14:02 +0900

    add snippet keymap

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -23,6 +23,9 @@ return {
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
+    local feedkey = function(key, mode)
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    end
 
     cmp.setup({
       snippet = {
@@ -55,6 +58,8 @@ return {
           i = function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
             elseif has_words_before() then
               cmp.mapping.complete()
             else
@@ -72,6 +77,8 @@ return {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
           else
             fallback()
           end
