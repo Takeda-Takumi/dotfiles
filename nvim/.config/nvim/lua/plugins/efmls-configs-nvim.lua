c06a35aabe29e0172613f6fb95f09fa17f4a5bae
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-18 15:03:28 +0900

    add linter

---
+++ nvim/.config/nvim/lua/plugins/efmls-configs-nvim.lua
@@ -0,0 +1,40 @@
+local M
+
+M = {
+  'creativenull/efmls-configs-nvim',
+  version = 'v1.x.x',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+  },
+  event = 'VeryLazy',
+}
+
+M.config = function()
+  require 'mason'
+  local languages = require('efmls-configs.defaults').languages()
+
+  local eslint_d = require('efmls-configs.linters.eslint_d')
+  local prettier = require('efmls-configs.formatters.prettier')
+
+  languages = vim.tbl_extend('force', languages, {
+    typescript = { eslint_d, prettier }
+  })
+
+  local efmls_config = {
+    filetypes = vim.tbl_keys(languages),
+    settings = {
+      rootMarkers = { '.git/' },
+      languages = languages,
+    },
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+    },
+  }
+  require('lspconfig').efm.setup(vim.tbl_extend('force', efmls_config, {
+
+  }))
+end
+
+return M
