8552b6d8853989e07cff94c792a711c53f05e9a6
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2023-11-21 11:46:44 +0900

    display macro at mini view

--- nvim/.config/nvim/lua/plugins/noice_nvim.lua
+++ nvim/.config/nvim/lua/plugins/noice_nvim.lua
@@ -1,4 +1,6 @@
-return {
+local M
+
+M = {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
@@ -12,23 +14,33 @@ return {
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
   },
-  config = function()
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        command_palette = true,       -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
-      },
-    })
-  end
 }
+
+M.config = function()
+  require("noice").setup({
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      command_palette = true,       -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,       -- add a border to hover docs and signature help
+    },
+
+    routes = {
+      {
+        view = 'mini',
+        filter = { event = 'msg_showmode', find = 'recording @' }
+      },
+    }
+  })
+end
+
+return M
