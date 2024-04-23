8a692f3bc5764420c07e77c41660efce1051670a
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2024-04-23 17:46:23 +0900

    modify comform.nvim

--- nvim/.config/nvim/lua/plugins/conform_nvim.lua
+++ nvim/.config/nvim/lua/plugins/conform_nvim.lua
@@ -1,7 +1,11 @@
-return {
+local M = {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  cmd = {
+    "ConformInfo",
+    "FormatDisable",
+    "FormatEnable"
+  },
   keys = {
     {
       -- Customize or remove this keymap to your liking
@@ -36,3 +40,36 @@ return {
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
 }
+
+M.config = function()
+  require('conform').setup({
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
+  })
+
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  })
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
+
+  })
+end
+
+return M
