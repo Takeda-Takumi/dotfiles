ed20248a8d582b6bfda9c5048e97093971c7bf86
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2024-08-16 13:37:33 +0900

    add nix flake

---
+++ nix/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    };
+}
