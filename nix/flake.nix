3013ed48064f3a16f29651f865e7a322d69e009d
Takeda-Takumi <91676218+Takeda-Takumi@users.noreply.github.com>
2024-11-03 21:10:10 +0900

    add nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,29 +1,41 @@
 {
-  description = "Home Manager configuration of takeda";
+  description = "Home Manager configuration";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."takeda" = home-manager.lib.homeManagerConfiguration {
+    in
+    {
+      homeConfigurations."myHomeConfig" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./.config/home-manager/home.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+      };
+      packages.x86_64-linux.my-packages = pkgs.buildEnv {
+        name = "my-packages-list";
+        paths = with pkgs; [
+          curl
+        ];
+      };
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [ cowsay ];
       };
     };
 }
